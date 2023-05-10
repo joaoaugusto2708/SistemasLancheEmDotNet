5e3bf7295f7d059904f801bf4ce6a482c9ccd798
João Augusto <joaorodriguesjf@hotmail.com>
2023-05-10 14:48:26 -0300

    50% do programa pronto

--- SistemasLanche/Startup.cs
+++ SistemasLanche/Startup.cs
@@ -14,8 +14,12 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))) ;
-        services.AddControllersWithViews();
+
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        services.AddTransient<ILancheRepository, LancheRepository>();
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+
+        IMvcBuilder mvcBuilder = services.AddControllersWithViews();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
